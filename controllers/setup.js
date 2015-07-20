2398eafc4c9382c40fe7b475009312e9594602e5
Marcus Noble <marcus.noble@stfc.ac.uk>
2015-07-20 15:05:51 +0100

    Populate list of repos

--- controllers/setup.js
+++ controllers/setup.js
@@ -5,7 +5,46 @@ var router = express.Router();
 var isAuthenticated = require('../middleware').isAuthenticated;
 
 router.get("/setup", isAuthenticated, function(req, res){
-
+    var github = GitHubApi.client(req.user.token);
+    var me = github.me();
+    var orgsArr = [];
+    var i = 0;
+    // Get all user's own repos
+    me.repos(function(err, repos){
+    	var repoArr = [];
+        repos.forEach(function(repo){
+            repoArr.push(repo.full_name);
+        });
+        orgsArr.push({
+            name: req.user.profile.username,
+            repos: repoArr
+        });
+        i++;
+    });
+    // Fetch all orgs repos
+    me.orgs(function(err, orgs){
+        if(err){
+            console.log(err);
+            res.sendStatus(err.statusCode);
+            return;
+        }
+        orgs.forEach(function(org){
+            var ghOrg = github.org(org.login);
+            ghOrg.repos(function(err, repos){
+                var repoArr = [];
+                repos.forEach(function(repo){
+                    repoArr.push(repo.full_name);
+                });
+                orgsArr.push({
+                    name: org.login,
+                    repos: repoArr
+                });
+                if(++i === (orgs.length+1)){
+                	res.render('setup', {orgs: orgsArr});
+                }
+            });
+        });
+    })
 });
 
 module.exports = router;
