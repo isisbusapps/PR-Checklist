43eed141d4d1bc141106d23bb3cf853c4ccb7fe0
Marcus Noble <marcus.noble@stfc.ac.uk>
2015-07-20 17:08:23 +0100

    Added header

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('*', function(req, res, next) {
+    if (req.isAuthenticated()) {
+        res.locals.user = req.user;
+    }
+
+    return next();
+});
+
 router.get('/', function(req, res){
     res.render('index');
 });
